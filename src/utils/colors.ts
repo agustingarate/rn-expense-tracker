e9f97a2ae4eb54e2500aa47cfdcb2cc5dd3fad20
Agustin Garate <agustin@spark6.com>
2024-07-22 13:35:37 -0300

    ADD colors' theme

---
+++ src/utils/colors.ts
@@ -0,0 +1,40 @@
+class Colors {
+  static primary = {
+    "400": "#853af5",
+    "700": "#4800e4",
+    "200": "#bb93f8",
+  };
+
+  static complementary = {
+    "400": "#abf53a",
+    "700": "#87c900",
+    "200": "#cffb95",
+  };
+
+  static analogousBlue = {
+    "500": "#3a4df5",
+    "200": "#a4aafc",
+    "800": "#202bd2",
+  };
+  static analogousPink = {
+    "300": "#e33af5",
+    "50": "#fbe2fd",
+    "800": "#8700d8",
+  };
+
+  static triadicPink = {
+    "300": "#f53aab",
+    "100": "#f8b3dc",
+    "700": "#cc0071",
+  };
+
+  static triadicYellow = {
+    "400": "#853af5",
+    "700": "#4800e4",
+    "200": "#bb93f8",
+  };
+
+  static others = {
+    red: "#f32d41",
+  };
+}
